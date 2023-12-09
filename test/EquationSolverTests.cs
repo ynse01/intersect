e8003812b430d4526b4eb110ae9f9a54d79400fc
Ynse Hoornenborg <github@ynse.nl>
2023-12-09 10:21:50 +0100

    Better solve tests

--- test/EquationSolverTests.cs
+++ test/EquationSolverTests.cs
@@ -5,52 +5,51 @@ namespace Intersect.Test {
     public class EquationSolverTests
     {
         [Test]
-        [TestCase(-2, 1, 1, 1, -0.5f)]
-        [TestCase(-4, 2, 6, -1, 1.5f)]
-        public void TestQuadratic2Roots(float a, float b, float c, float root0, float root1)
+        [TestCase(-2, 1)]
+        [TestCase(-4, 2)]
+        [TestCase(2, 4)]
+        [TestCase(2, 1)]
+        public void TestLinear(float a, float b)
         {
             // Arrange
-            float[] expected = new float[] { root0, root1};
             // Act
-            var actual = EquationSolver.SolveQuadratic(a, b, c);
+            var actual = EquationSolver.SolveLinear(a, b);
             // Assert
-            CollectionAssert.AreEquivalent(actual, expected);
+            var y = a * actual + b;
+            Assert.AreEqual(0.0f, y);
         }
 
         [Test]
-        [TestCase(2, 4, 2, -1)]
-        public void TestQuadratic1Root(float a, float b, float c, float root)
+        [TestCase(-2, 1, 1, 2)]
+        [TestCase(-4, 2, 6, 2)]
+        [TestCase(2, 4, 2, 1)]
+        [TestCase(2, 1, 9, 0)]
+        public void TestQuadratic(float a, float b, float c, int numRoots)
         {
             // Arrange
-            float[] expected = new float[] { root};
             // Act
             var actual = EquationSolver.SolveQuadratic(a, b, c);
             // Assert
-            CollectionAssert.AreEquivalent(actual, expected);
-        }
-
-        [Test]
-        [TestCase(2, 1, 9)]
-        public void TestQuadraticComplexRoots(float a, float b, float c)
-        {
-            // Arrange
-            float[] expected = new float[0];
-            // Act
-            var actual = EquationSolver.SolveQuadratic(a, b, c);
-            // Assert
-            CollectionAssert.AreEquivalent(actual, expected);
+            Assert.AreEqual(numRoots, actual.Count);
+            foreach(var x in actual) {
+                var y = a * x * x + b * x + c;
+                Assert.AreEqual(0.0f, y);
+            }
         }
 
         [Test]
         [TestCase(1, -3, 3, -1, 1)]
-        public void TestQubic3Roots(float a, float b, float c, float d, float root0)
+        public void TestQubic(float a, float b, float c, float d, int numRoots)
         {
             // Arrange
-            float[] expected = new float[] { root0 };
             // Act
             var actual = EquationSolver.SolveQubic(a, b, c, d);
             // Assert
-            CollectionAssert.AreEquivalent(actual, expected);
+            Assert.AreEqual(numRoots, actual.Count);
+            foreach(var x in actual) {
+                var y = a * x * x * x+ b * x * x + c * x + d;
+                Assert.AreEqual(0.0f, y);
+            }
         }
     }
 }
