b56f6b6b9118838c3fd05d719ed9ce33742e1aee
Ynse Hoornenborg <github@ynse.nl>
2023-12-10 10:09:41 +0100

    Correct cubic solve

--- src/EquationSolver.cs
+++ src/EquationSolver.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Intersect {
 
@@ -27,38 +28,44 @@ namespace Intersect {
             // Taken from: https://stackoverflow.com/questions/49671821/implementing-cardano-method-for-a-cubic-equation-solution
             List<float> output = new List<float>();
             if (a != 0) {
-                float a1 = b / a;
-                float a2 = c / a;
-                float a3 = d / a;
-                float p = -(a1 * a1 / 3.0f) + a2;
-                float q = (2.0f * a1 * a1 * a1 / 27.0f) - (a1 * a2 / 3.0f) + a3;
-                float cubeDiscr = q * q / 4.0f + p * p * p / 27.0f;
+                double a1 = b / a;
+                double a2 = c / a;
+                double a3 = d / a;
+                double p = a2 - (a1 * a1 / 3d);
+                double q = (2d * a1 * a1 * a1 / 27d) - (a1 * a2 / 3d) + a3;
+                double cubeDiscr = q * q / 4d + p * p * p / 27d;
                 if (cubeDiscr > 0) {
-                    float u = CubicRoot(-q / 2.0 + Math.Sqrt(cubeDiscr));
-                    float v = CubicRoot(-q / 2.0 - Math.Sqrt(cubeDiscr));
-                    output.Add(u + v - (a1 / 3.0f));
+                    double u = CubicRoot(-q / 2d + Math.Sqrt(cubeDiscr));
+                    double v = CubicRoot(-q / 2d - Math.Sqrt(cubeDiscr));
+                    output.Add((float)(u + v - (a1 / 3.0f)));
                 }
                 else if (cubeDiscr == 0) {
-                    float u = CubicRoot(-q / 2.0f);
+                    double u = CubicRoot(-q / 2d);
                     if (u != 0.0f) {
                         // Prevent duplicate result.
-                        output.Add(2.0f * u - (a1 / 3.0f));
+                        output.Add((float)(2d * u - (a1 / 3d)));
                     }
-                    output.Add(-u - (a1 / 3.0f));
+                    output.Add((float)(-u - (a1 / 3d)));
                 }
                 else if (cubeDiscr < 0) {
-                    float r = CubicRoot(Math.Sqrt(-(p * p * p / 27.0f)));
-                    float alpha = (float)Math.Atan(Math.Sqrt(-cubeDiscr) / (-q / 2.0));
-                    output.Add(r * (float)(Math.Cos(alpha / 3.0f) + Math.Cos((6.0f * Math.PI - alpha) / 3.0f)) - a1 / 3.0f);
-                    output.Add(r * (float)(Math.Cos((2.0f * Math.PI + alpha) / 3.0f) + Math.Cos((4.0f * Math.PI - alpha) / 3.0)) - a1 / 3.0f);
-                    output.Add(r * (float)(Math.Cos((4.0f * Math.PI + alpha) / 3.0f) + Math.Cos((2.0f * Math.PI - alpha) / 3.0)) - a1 / 3.0f);
+                    //float r = CubicRoot(Math.Sqrt(-(p * p * p / 27.0f)));
+                    //float alpha = (float)Math.Atan(Math.Sqrt(-cubeDiscr) / (-q / 2.0));
+                    //output.Add(r * (float)(Math.Cos(alpha / 3.0f) + Math.Cos((6.0f * Math.PI - alpha) / 3.0f)) - a1 / 3.0f);
+                    //output.Add(r * (float)(Math.Cos((2.0f * Math.PI + alpha) / 3.0f) + Math.Cos((4.0f * Math.PI - alpha) / 3.0)) - a1 / 3.0f);
+                    //output.Add(r * (float)(Math.Cos((4.0f * Math.PI + alpha) / 3.0f) + Math.Cos((2.0f * Math.PI - alpha) / 3.0)) - a1 / 3.0f);
+                    double u = (2d * Math.Sqrt(p / -3d));
+                    double theta = Math.Acos((3d * q) * Math.Sqrt(-3d / p) / (2d * p)) / 3d;
+                    for(int i = 0; i < 3; i++) {
+                        double t = (u * Math.Cos(theta - 2d * Math.PI * i / 3d));
+                        output.Add((float)(t - a1 / 3d));
+                    }
                 }
             }
             return output;
         }
 
-        private static float CubicRoot(double n) {
-            return (float)(Math.Pow(Math.Abs(n), 1f / 3f) * Math.Sign(n));
+        private static double CubicRoot(double n) {
+            return (Math.Pow(Math.Abs(n), 1d / 3d) * Math.Sign(n));
         }
     }
 }
--- test/EquationSolverTests.cs
+++ test/EquationSolverTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace Intersect.Test {
@@ -15,8 +18,7 @@ namespace Intersect.Test {
             // Act
             var actual = EquationSolver.SolveLinear(a, b);
             // Assert
-            var y = a * actual + b;
-            Assert.AreEqual(0.0f, y);
+            AssertResults(new [] { actual }, (x) => a * x + b);
         }
 
         [Test]
@@ -31,14 +33,13 @@ namespace Intersect.Test {
             var actual = EquationSolver.SolveQuadratic(a, b, c);
             // Assert
             Assert.AreEqual(numRoots, actual.Count);
-            foreach(var x in actual) {
-                var y = a * x * x + b * x + c;
-                Assert.AreEqual(0.0f, y);
-            }
+            AssertResults(actual, (x) => a * x * x + b * x + c);
         }
 
         [Test]
         [TestCase(1, -3, 3, -1, 1)]
+        [TestCase(1, -7, 7, 15, 3)]
+        [TestCase(1, -5, -2, 24, 3)]
         public void TestQubic(float a, float b, float c, float d, int numRoots)
         {
             // Arrange
@@ -46,10 +47,17 @@ namespace Intersect.Test {
             var actual = EquationSolver.SolveQubic(a, b, c, d);
             // Assert
             Assert.AreEqual(numRoots, actual.Count);
-            foreach(var x in actual) {
-                var y = a * x * x * x+ b * x * x + c * x + d;
-                Assert.AreEqual(0.0f, y);
+            AssertResults(actual, (x) => a * x * x * x + b * x * x + c * x + d);
+        }
+
+        private void AssertResults(IList<float> results, Func<float, float> expected) {
+            List<float> actual = new List<float>();
+            List<float> zeros = new List<float>();
+            foreach(var x in results) {
+                actual.Add(expected(x));
+                zeros.Add(0f);
             }
+            CollectionAssert.AreEqual(zeros, actual);
         }
     }
 }
